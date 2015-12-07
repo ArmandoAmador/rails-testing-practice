dc4c5d5d5e67726d61f5f48d52719899d61255d3
Armando Amador <arm.amador@gmail.com>
2015-12-06 23:28:18 -0500

    generate zombie and tweet scaffold
     - write fixtures for zombies and tweets
     - write model test for zombies

--- app/models/string_extension.rb
+++ app/models/string_extension.rb
@@ -3,11 +3,11 @@ class String
     self =~ /^\d+$/
   end
 
-  def humanize
-    if self =~ /zombie/
-      raise RuntimeError
-    else
-      downcase.capitalize
-    end
-  end
+  # def humanize
+  #   if self =~ /zombie/
+  #     raise RuntimeError
+  #   else
+  #     downcase.capitalize
+  #   end
+  # end
 end
--- test/models/string_extension_test.rb
+++ test/models/string_extension_test.rb
@@ -10,23 +10,23 @@ class StringExtensionTest < ActiveSupport::TestCase
     assert !'Blah'.number?
   end
 
-  test 'test_humanize_function_added_to_string' do
-    assert_respond_to 'blah', :humanize
-  end
+  # test 'test_humanize_function_added_to_string' do
+  #   assert_respond_to 'blah', :humanize
+  # end
 
-  test 'test_humanize_returns_something' do
-    assert_not_nil 'Yo'.humanize, 'humanize is returning nil'
-  end
+  # test 'test_humanize_returns_something' do
+  #   assert_not_nil 'Yo'.humanize, 'humanize is returning nil'
+  # end
 
-  test 'test_humanize' do
-    assert_equal 'Likes me brains!', 'LIKES ME BRAINS!'.humanize
-  end
+  # test 'test_humanize' do
+  #   assert_equal 'Likes me brains!', 'LIKES ME BRAINS!'.humanize
+  # end
 
-  test 'test_just_for_brains' do
-    assert_match(/brains/, 'LIKES ME BRAINS!'.humanize)
-  end
+  # test 'test_just_for_brains' do
+  #   assert_match(/brains/, 'LIKES ME BRAINS!'.humanize)
+  # end
 
-  test 'test_zombie_in_humanize_raises_error' do
-    assert_raise(RuntimeError) { 'zombie'.humanize }
-  end
+  # test 'test_zombie_in_humanize_raises_error' do
+  #   assert_raise(RuntimeError) { 'zombie'.humanize }
+  # end
 end
