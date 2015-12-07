b85cf2bd072cf52f18181aa94d24fca35c0c87b9
Armando Amador <arm.amador@gmail.com>
2015-12-06 23:54:17 -0500

    add basic model test to tweet model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,7 @@
 class Tweet < ActiveRecord::Base
   belongs_to :zombie
+
+  def brains?
+    status =~ /(brains|breins)/
+  end
 end
--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class TweetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid with all attributes' do
+    tweet = tweets(:ash_1)
+    assert tweet.valid?, "tweet isn't valid"
+  end
+
+  test 'can detect brains' do
+    tweet = tweets(:ash_1)
+    tweet.status = 'brains'
+    assert tweet.brains?
+  end
+
+  test 'contains a zombie' do
+    tweet = tweets(:ash_1)
+    zombie = zombies(:ash)
+    zombie.tweets.all? { |t| t.zombie == zombie }
+    assert tweet.zombie == zombie
+  end
 end
