9c4d05133907dbdd7a77c243b6dc500dfb43dd14
Armando Amador <arm.amador@gmail.com>
2015-12-06 22:32:48 -0500

    add humanize method to string extension

--- app/models/string_extension.rb
+++ app/models/string_extension.rb
@@ -2,4 +2,12 @@ class String
   def number?
     self =~ /^\d+$/
   end
+
+  def humanize
+    if self =~ /zombie/
+      raise RuntimeError
+    else
+      downcase.capitalize
+    end
+  end
 end
